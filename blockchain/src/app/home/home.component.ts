b399cd36d48395564304088ae083f20ec3fbed1b
rckam <rckmiri@gmail.com>
2022-11-17 13:45:42 +0100

    updated home page

--- blockchain/src/app/home/home.component.ts
+++ blockchain/src/app/home/home.component.ts
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const video = document.getElementById("background-video") as HTMLVideoElement;
+    video.play().then(r => {});
   }
 
 }
