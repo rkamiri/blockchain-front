374c0f07f0b86cf410ffe96eec276084cd7ccf58
rckam <rckamiri@gmail.com>
2022-11-18 19:07:07 +0100

    updated home page

--- blockchain/src/app/home/home.component.ts
+++ blockchain/src/app/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('scroll', [
+      state('on', style({left: '-100px'})),
+      transition('* => *', [
+        style({left: '-100px'}),
+        animate(10000, style({left: '100%'}))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+  state = 0;
+
+  scrollDone() {
+    this.state++;
+  }
 
   constructor() { }
 
